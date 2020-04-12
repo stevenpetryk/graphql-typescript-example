a49b13418219af40b7a039e520e8ac853cf2a109
Steven Petryk <petryk.steven@gmail.com>
2020-04-12 13:23:25 -0700

    Install Apollo client, set up first query

--- app/javascript/packs/application.ts
+++ app/javascript/packs/application.ts
@@ -1,6 +1,30 @@
+import { ApolloClient, HttpLink, InMemoryCache, gql } from "@apollo/client";
+
 require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 
-console.log("Hello from the console!");
+const railsCsrfToken = document
+  .querySelector("meta[name=csrf-token]")
+  ?.getAttribute("content");
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "http://localhost:3000/graphql",
+    headers: {
+      "X-CSRF-Token": railsCsrfToken,
+    },
+  }),
+});
+
+const GET_TEST = gql`
+  query Test {
+    testField
+  }
+`;
+
+client
+  .query({ query: GET_TEST })
+  .then((result) => console.log("GraphQL result: ", result.data));
